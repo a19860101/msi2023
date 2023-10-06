f3f3a2f6e0c97c67a640b866e5fb54134ee255aa
Zac <a19860101@gmail.com>
2023-10-06 20:14:03 +0800

    20231006

--- VUE-COMPONENT/App.js
+++ VUE-COMPONENT/App.js
@@ -1,7 +1,14 @@
+import Btn from './components/Btn.js';
+import Nav from './components/Nav.js';
 export default {
     data(){
         return {
             title: 'Hello VUE'
         }
+    },
+    components:{
+        // 'Btn':Btn
+        Btn,
+        Nav
     }
 }
---
+++ VUE-COMPONENT/components/Nav.js
@@ -0,0 +1,9 @@
+export default {
+    template:`
+        <nav>
+            <a href="#">About</a>
+            <a href="#">Service</a>
+            <a href="#">Contact</a>
+        </nav>
+    `
+}
