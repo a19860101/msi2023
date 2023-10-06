a0efc11fff6b53d515e5ff3d28487765a709bd37
Zac <a19860101@gmail.com>
2023-10-06 21:45:51 +0800

    20231006

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  
+  <Title>Hello VITE</Title>
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
-
+  import Title from './components/Title.vue';
+  export default {
+    data(){
+      return {
+        title: 'hello vite'
+      }
+    },
+    components:{
+      // 'Title':Title
+      Title
+    }
+  }
 </script>
 
 <style>
---
+++ vite-project/src/components/Title.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1><slot /></h1>
+    
+</template>
+
+<style scoped>
+
+</style>
